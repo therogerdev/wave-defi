2a8021292e85eaea236e88f806d4a26f71b8ec01
Rogerio Menezes <rogeriomenezes.nz@gmail.com>
2025-02-23 14:35:20 +0700

    frontend color scheme

--- client/src/components/ThemeToggle.tsx
+++ client/src/components/ThemeToggle.tsx
@@ -15,12 +15,11 @@ import {
 export function ThemeToggle({ type }: { type?: "compact" | "full" }) {
   const { theme, setTheme } = useTheme();
 
-
   if (type === "full") {
     return (
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Button  variant="outline" size="icon">
+          <Button variant="outline" size="icon">
             <Sun className="h-[1.2rem] text-foreground w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
             <Moon className="absolute text-foreground h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
             <span className="sr-only">Toggle theme</span>
