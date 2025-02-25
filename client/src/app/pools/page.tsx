4a6143569a629cef3fdb7bec44398ee44cb6871d
Rogerio Menezes <rogeriomenezes.nz@gmail.com>
2025-02-25 20:03:50 +0700

    pool item card and list

--- client/src/app/pools/page.tsx
+++ client/src/app/pools/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { PageWrapper } from "@/components/PageWrapper";
+import PoolItem from "@/components/PoolItem";
 import { pairListAtom } from "@/store/pairListAtom";
 import { useAtomValue } from "jotai";
 import * as React from "react";
@@ -9,11 +10,10 @@ export default function PoolPage() {
 
   return (
     <PageWrapper>
-      <h1 className="text-2xl font-bold mb-4">Available Liquidity Pools</h1>
       {tokenPairs.length === 0 ? (
         <p>No pools found.</p>
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
           {tokenPairs.map((pair, index) => (
             <PoolItem
               key={index}
@@ -26,18 +26,3 @@ export default function PoolPage() {
     </PageWrapper>
   );
 }
-
-interface PoolItemProps {
-  tokenA: string;
-  tokenB: string;
-}
-
-const PoolItem: React.FC<PoolItemProps> = ({ tokenA, tokenB }) => {
-  return (
-    <div className="border p-4 rounded-lg shadow-md">
-      <p className="font-semibold">
-        {tokenA} / {tokenB}
-      </p>
-    </div>
-  );
-};
--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,6 +1,30 @@
 import { clsx, type ClassValue } from "clsx";
+import { toast } from "sonner";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const getInitials = (firstName: string, lastName: string) => {
+  const firstInitial = firstName ? firstName[0] : "";
+  const lastInitial = lastName ? lastName[0] : "";
+
+  return `${firstInitial}${lastInitial}`;
+};
+
+/**
+ * Copies a given text to the clipboard and shows a toast notification.
+ * @param text The text to copy to clipboard.
+ * @param label A label to display in the toast notification (e.g., "Token", "Address").
+ */
+
+export const handleCopy = (text: string, label: string = "Copied") => {
+  if (!text) return;
+
+  navigator.clipboard.writeText(text);
+  toast("Copied to clipboard", {
+    description: `${label}: ${text}`,
+    position: "top-center",
+  });
+};
